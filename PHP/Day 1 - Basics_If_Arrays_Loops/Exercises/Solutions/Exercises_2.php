55502d8e9f9f63160d51a965aace28bab37eee63
simon <fctinstean@gmail.com>
2021-10-18 15:37:00 +0200

    solution exercise 2

--- PHP/Day 1 - Basics_If_Arrays_Loops/Exercises/Solutions/Exercises_2.php
+++ PHP/Day 1 - Basics_If_Arrays_Loops/Exercises/Solutions/Exercises_2.php
@@ -1,35 +1,45 @@
 <?php
 
-/*
+$avatar = 'banana.png';
+$lastName = 'Guy';
+$firstName = 'Banana';
 
--- Exercise 5
+$characteristics = array(
+    'atk' => 10,
+    'def' => 5
+);
 
-- Part 1 :
-    You need to create a web page with basics HTML tags.
-    In this page, you'll display all the characteristics of an imaginary character, from a role-play game.
+?>
 
-        The character information will be saved in different variables.
-        There is four (4) informations :
-        -- An avatar image
-        -- The last name
-        -- The first name
-        -- An array of characteristics (like attak point, defense point... put anything you want)
-        Use an associative array.
+<!DOCTYPE html>
+<html lang="en">
 
-*/
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Character page</title>
+</head>
 
-/*
+<body>
+    <h2>Character</h2>
 
-- Part 2:
-	Based on the 'character' exercise, make sure that, if its characteristic "Attak" or "Defense" are greater than 9, an additional message is displayed:
+    <p><strong>Last name : </strong> <?php echo $lastName; ?> </p>
+    <p><strong>First name : </strong> <?= $firstName; ?> </p>
 
-	<div class="alert">
-    	<strong>Congratulations !</strong> Your character is ready to fight.</strong>
-	</div>
+    <img src="<?php echo $avatar; ?>" alt="" width="100px">
 
-    Add a conddition, if it is below 5, display message :
+    <?php
 
-    <div class="alert">
-        <strong>Wait !</strong> Your charachter is too weeeakk!
-    </div>
-*/
+    if ($characteristics['atk'] > 9 or $characteristics['def'] > 9) {
+        echo '<div class="alert">
+                <strong>Congratulations !</strong> Your character is ready to fight.</strong>
+            </div>';
+    }
+
+    ?>
+
+
+</body>
+
+</html>
