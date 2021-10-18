085ca5fd62e68030b1e0143c4bf56566a5909af4
simon <fctinstean@gmail.com>
2021-10-18 14:58:42 +0200

    exo 2

---
+++ PHP/Day 1 - Basics_If_Arrays_Loops/Exercises/Solutions/Exercises_2.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+
+-- Exercise 5
+
+- Part 1 :
+    You need to create a web page with basics HTML tags.
+    In this page, you'll display all the characteristics of an imaginary character, from a role-play game.
+
+        The character information will be saved in different variables.
+        There is four (4) informations :
+        -- An avatar image
+        -- The last name
+        -- The first name
+        -- An array of characteristics (like attak point, defense point... put anything you want)
+        Use an associative array.
+
+*/
+
+/*
+
+- Part 2:
+	Based on the 'character' exercise, make sure that, if its characteristic "Attak" or "Defense" are greater than 9, an additional message is displayed:
+
+	<div class="alert">
+    	<strong>Congratulations !</strong> Your character is ready to fight.</strong>
+	</div>
+
+    Add a conddition, if it is below 5, display message :
+
+    <div class="alert">
+        <strong>Wait !</strong> Your charachter is too weeeakk!
+    </div>
+*/
