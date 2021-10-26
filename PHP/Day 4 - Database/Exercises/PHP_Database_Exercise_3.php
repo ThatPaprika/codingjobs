69aa7edefc63a7224f3e8dd643fef9b090aaf8e5
simon <fctinstean@gmail.com>
2021-10-26 14:15:37 +0200

    Bonus exercise

---
+++ PHP/Day 4 - Database/Exercises/PHP_Database_Exercise_3.php
@@ -0,0 +1,32 @@
+<?php
+/*
+	Let's continue our movie website.
+
+
+	BONUS EXERCISE 1 : 
+
+		0. Add a picture attribute on the directors table (you can skip this).
+
+		1. Create a page 'directors.php'
+		This page will display all the directors 
+		For each directors, you have to display : Picture, name, nationality and number of movies.
+	
+		2. Create a page 'director.php'.
+		This page is a descriptive page for each director. It'll display the picture, name and nationality of the director.
+		It'll also display ALL the movies from this director (at least the title).
+		This page will have to use the GET method to get the id of the director you want to display.
+		So in your adress bar it'll look like this : 'localhost/director.php?id=1'
+
+	BONUS EXERCISE 2 :
+		
+		1. Create a new page 'new-movie.php'
+
+		2. Create a form on this page to ask for every movie's information (title, views, poster and director_id).
+
+		3. When submitting the form you should : 
+			- Make sure every input is not empty.
+			- Save the movie inside the DB.
+
+		At the end, this form should be used to insert new movie in the DB.
+
+ */
