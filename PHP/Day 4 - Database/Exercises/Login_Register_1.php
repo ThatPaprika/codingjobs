0accb00c5abac132efd86a54ea360aebd02c7fab
simon <fctinstean@gmail.com>
2021-10-26 14:38:09 +0200

    Register/Login

---
+++ PHP/Day 4 - Database/Exercises/Login_Register_1.php
@@ -0,0 +1,34 @@
+<?php
+/*
+	Let's assume you have an online shopping website.
+
+    In this exercise, you'll have to create a register/login process.
+    
+    - Step 0 :
+		Import the 'users.sql' file into PHPMYADMIN.
+		You have to choose a DB first (you can use movie_db or create a new one).
+
+		Some users will be imported into your database.
+		You can insert users manually from PHPMYADMIN interface if you want.
+    
+    - Step 1 :
+        Create a 'register.php' file
+        This file contains a form with those input :
+            * username
+            * email
+            * password
+            * confirm password
+            * submit btn
+    
+    - Step 2 :
+        If the form is submitted, you have to check :
+            * username, mail and password must not be empty !
+            * mail must be a valid one
+            * passwords must be the same 
+
+        Display a different message for each validation/error.
+
+    - Step 3 :
+        If everything is good, insert the new user in the DB !
+
+*/
---
+++ PHP/Day 4 - Database/Exercises/Login_Register_2.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+    Let's continue our login/register process.
+
+    Step 1: 
+	    Create a page 'login.php'.
+	    Inside this page, create a form to login into your beautiful website.
+
+	    The form will ask for : email, password.
+
+    Step 2: 
+		Once the user submit the login form, you have to :
+			- Check if email and password are not empty.
+			- Check if user exists in DB
+            - Check if password match the email credentials
+
+
+        If you found the user, display a message 'successfully logged in'
+
+ */
