407552f9b1666234b7db60bc16425f990df74873
simon <fctinstean@gmail.com>
2021-10-29 14:20:08 +0200

    Injection

---
+++ PHP/Day 6 - Encryption_Injection_Upload/Exercises/Injection_Exercise.php
@@ -0,0 +1,9 @@
+<?php
+
+/*
+	
+	Based on the 'Register_Exercise' and 'Login_Exercise'.
+
+	Put in place a basic security check in the login/register pages.
+	Remove white spaces, Avoid injections, special chars....
+*/
