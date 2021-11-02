012c4f8f837c58347de40ce3155808b54e52e14d
simon <fctinstean@gmail.com>
2021-11-02 14:45:35 +0100

    More exercise

---
+++ PHP/Day 7 - Pagination/Exercise/Movie_Exercise_1.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+	This exercise is based on the 'movies' exercise.
+	
+	Now, we have implemented the register/login process into the same project.
+
+	Step 1. Open 'nav.html' and rename it to 'nav.php'
+	Step 2. If the user is loggued-in, hide 'login' & 'register' links and display 'account' instead.
+	Step 3. If the user is not loggued-in, display 'login' & 'register' links.
+	
+?>
