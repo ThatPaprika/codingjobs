2cfe08036094f5779185b043c77e1142366beb1b
simon <fctinstean@gmail.com>
2021-11-02 16:14:48 +0100

    solution

---
+++ PHP/Day 7 - Pagination/Exercise/Solution/nav.php
@@ -0,0 +1,22 @@
+<nav>
+  <ul>
+    <li>
+      <a href="index.php">Home</a>
+    </li>
+    <li>
+      <a href="movies.php">Movies List</a>
+    </li>
+    <li>
+      <a href="login.php">Log In</a>
+    </li>
+    <li>
+      <a href="register.php">Register</a>
+    </li>
+    <li>
+      <a href="account.php">Account page</a>
+    </li>
+    <li>
+      Amount : <?= $amount; ?>
+    </li>
+  </ul>
+</nav>
