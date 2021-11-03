0b530cfbadfd9155588435dccef60f55cf8843f5
simon <fctinstean@gmail.com>
2021-11-03 14:30:54 +0100

    Create Composer_Exercise.php

---
+++ PHP/Day 8 - AJAX_Composer/Exercises/Composer_Exercise.php
@@ -0,0 +1,33 @@
+<?php 
+
+/*
+	-- Exercise : 
+
+	This exercise is to get use to composer.
+
+	Composer is a free dependency manager for PHP libraries
+
+	- Step 1 :
+	
+	Get composer here : https://getcomposer.org/
+	Go to download page, download the windows executable and install it.
+
+	You can enable developer mode but don't change the other options.
+
+	- Step 2 : 
+
+	Let's try to create a project using composer and a package to send EMAIL.
+
+	Go to this link : https://github.com/PHPMailer/PHPMailer and read the instruction on 'Installation & Loading' section.
+
+	- Step 3 :
+
+	Create a contact.php page with some input (email, subject, message...)
+
+	- Step 4 : 
+
+	Read section 'A simple example' on PHPMAILER official github repo.
+	Try to implement this simple example to send an email.
+
+
+*/
