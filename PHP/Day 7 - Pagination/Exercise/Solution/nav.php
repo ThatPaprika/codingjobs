123a64bd316d02fdd67534ba8a8b91dfcc56f2bd
simon <fctinstean@gmail.com>
2021-11-03 14:24:00 +0100

    Update nav.php

--- PHP/Day 7 - Pagination/Exercise/Solution/nav.php
+++ PHP/Day 7 - Pagination/Exercise/Solution/nav.php
@@ -6,15 +6,20 @@
     <li>
       <a href="movies.php">Movies List</a>
     </li>
-    <li>
-      <a href="login.php">Log In</a>
-    </li>
-    <li>
-      <a href="register.php">Register</a>
-    </li>
-    <li>
-      <a href="account.php">Account page</a>
-    </li>
+    <?php if (!isset($_SESSION['email'])) : ?>
+      <li>
+        <a href="login.php">Log In</a>
+      </li>
+      <li>
+        <a href="register.php">Register</a>
+      </li>
+    <?php endif; ?>
+
+    <?php if (isset($_SESSION['email'])) : ?>
+      <li>
+        <a href="account.php">Account page</a>
+      </li>
+    <?php endif; ?>
     <li>
       Amount : <?= $amount; ?>
     </li>
