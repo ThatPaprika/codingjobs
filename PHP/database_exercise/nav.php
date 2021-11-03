f1a3088560fa6583b8a3f1e52fa687e3cb789a43
simon <fctinstean@gmail.com>
2021-11-03 14:23:24 +0100

    Update database exercise

--- PHP/database_exercise/nav.php
+++ PHP/database_exercise/nav.php
@@ -7,14 +7,22 @@
       <a href="movies.php">Movies List</a>
     </li>
     <li>
-      <a href="login.php">Log In</a>
+      <a href="new-movie.html">New Movie</a>
     </li>
-    <li>
-      <a href="register.php">Register</a>
-    </li>
-    <li>
-      <a href="account.php">Account page</a>
-    </li>
+    <?php if (!isset($_SESSION['email'])) : ?>
+      <li>
+        <a href="login.php">Log In</a>
+      </li>
+      <li>
+        <a href="register.php">Register</a>
+      </li>
+    <?php endif; ?>
+
+    <?php if (isset($_SESSION['email'])) : ?>
+      <li>
+        <a href="account.php">Account page</a>
+      </li>
+    <?php endif; ?>
     <li>
       Amount : <?= $amount; ?>
     </li>
