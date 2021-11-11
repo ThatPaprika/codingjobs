63549c4fe461b61190f09d4d761d53415093e73d
simon <fctinstean@gmail.com>
2021-11-11 10:26:42 +0100

    Update AJAX_Exercise_5.php

--- PHP/Day 8 - AJAX_Composer/Exercises/AJAX_Exercise_5.php
+++ PHP/Day 8 - AJAX_Composer/Exercises/AJAX_Exercise_5.php
@@ -1,3 +1,25 @@
 <?php 
 
 /*
+	-- Exercise : 
+
+	This exercise is to get use to composer.
+
+	- Step 1 :
+
+	Create a new folder and open the console/terminal inside.
+	Enter this command : composer require phpmailer/phpmailer
+
+	- Step 2 :
+
+	Create a 'contact.php' page with some input (email, subject, message...)
+
+	- Step 3 : 
+
+	When clicking on the submit button, you should do an AJAX call to 'send-mail.php' file
+	This file should handle sending a mail !
+
+	- Step 4 : 
+
+	Display the informations on the page to inform the user.
+*/
