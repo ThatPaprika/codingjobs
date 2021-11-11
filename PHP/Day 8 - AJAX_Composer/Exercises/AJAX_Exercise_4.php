99e3cb274ccd17a9d6885d87e978cb1e6e7d043b
simon <fctinstean@gmail.com>
2021-11-11 10:22:30 +0100

    Ajax exercise

---
+++ PHP/Day 8 - AJAX_Composer/Exercises/AJAX_Exercise_4.php
@@ -0,0 +1,21 @@
+<?php 
+
+/*
+
+	-- Exercise : 
+
+	In this exercise, you will use the 'movie_db' database.
+
+	- Step 1 : Create two pages: 'update-form.html' and 'update-movie.php'
+	
+	- Step 2 : 'update-form.html' should expect an id in the URL.
+	Use GET Method.
+	The page should contain a form to update a movie.
+	
+	- Step 3 : When loading the page, look for movie's information and load it into the form.
+	
+	- Step 4 : When you submit the form, create an AJAX call on 'update-movie.php' to update the movie.
+	
+
+
+*/
