28fe451500cf1ce30756a5b8be62b902f40da591
simon <fctinstean@gmail.com>
2021-11-23 12:05:07 +0100

    Movie

--- PHP - OOP/Day 5 - JSON_Namespace/JSON/Movie.php
+++ PHP - OOP/Day 5 - JSON_Namespace/JSON/Movie.php
@@ -1,21 +1,13 @@
 <?php
 
-class Movie implements JsonSerializable
+class Movie
 {
-  private $title;
-  private $release_year;
+  public $title;
+  public $release_year;
 
   public function __construct($title, $release_year)
   {
     $this->title = $title;
     $this->release_year = $release_year;
-  }
-
-  // Override the method from JsonSerializable
-  public function jsonSerialize()
-  {
-    return [
-      "title" => $this->title
-    ];
   }
 }
