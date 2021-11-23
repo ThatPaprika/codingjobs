0aa6c7f2012429f18c4feaad5397f8332b4974e7
simon <fctinstean@gmail.com>
2021-11-23 10:06:57 +0100

    Exercise

---
+++ PHP - OOP/Day 5 - JSON_Namespace/Exercises/OOP_Json_Exercise.php
@@ -0,0 +1,18 @@
+<?php 
+
+/*
+	Exercise : 
+
+	Step 1 - Create a form that ask the user to enter a name and an email.
+
+	Step 2 - Create a class User. It have two properties : name and mail.
+
+	Step 3 - When the user enter informations in the form, you sould create a User object with those datas.
+
+	Step 4 - Save your user in a JSON file  :
+		-- Convert the User object into a JSON Format.
+		-- Save it in a JSON file and display 'Welcome, new user'
+
+
+ ?>
+
