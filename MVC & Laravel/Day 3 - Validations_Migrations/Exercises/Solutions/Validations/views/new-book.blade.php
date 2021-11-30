007b22d963b6133ce0cb7ee23ef123c4e38999e2
Simon <fctinstean@gmail.com>
2021-11-30 14:03:38 +0100

    Validation

---
+++ MVC & Laravel/Day 3 - Validations_Migrations/Exercises/Solutions/Validations/views/new-book.blade.php
@@ -0,0 +1,23 @@
+@extends('mytemplate')
+
+@section('title', 'Inserting a book')
+
+@section('content')
+    {{-- Comment using blade --}}
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    <form method="POST">
+        @csrf
+        <input type="text" name="title" placeholder="Title"><br>
+        <input type="text" name="price" placeholder="Price"><br>
+        <input type="submit" value="Insert a new book">
+    </form>
+@endsection
