5158240d91e1661e95f96c6a4dc4ef02542e67bc
Simon <fctinstean@gmail.com>
2021-11-30 14:57:47 +0100

    exercise

---
+++ MVC & Laravel/Day 3 - Validations_Migrations/Exercises/Migration_exercise.php
@@ -0,0 +1,25 @@
+
+<?php
+
+/*
+Continue the Library exercise.
+
+You have to declare your DB structure using migrations.
+Resource = https://laravel.com/docs/8.x/migrations
+
+Step 1 :
+
+	You have to create a books migration with necessary attributes.
+	Create also a BookSeeder to populate basic values
+
+Step 2 :
+
+	Create an authors migration.
+	This table will contain : id, last_name, first_name
+
+Step 3 : 
+
+	Edit the books structure.
+	Now it should include a foreign key : author_id
+
+*/
