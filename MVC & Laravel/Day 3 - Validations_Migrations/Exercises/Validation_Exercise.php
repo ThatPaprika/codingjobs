f9ce2a7d95fb49d9d54cba234ae5089e1b92d165
simon <fctinstean@gmail.com>
2021-11-30 11:42:20 +0100

    Update Validation_Exercise.php

--- MVC & Laravel/Day 3 - Validations_Migrations/Exercises/Validation_Exercise.php
+++ MVC & Laravel/Day 3 - Validations_Migrations/Exercises/Validation_Exercise.php
@@ -20,6 +20,8 @@ Step 1 :
 
 Step 2 : 
 
+    For this step, you have to read this section : https://laravel.com/docs/8.x/validation#form-request-validation
+
     You should customize the error message.
     If the title is empty, you should display 'Title is mandatory !'
     If the price is not a numeric value OR is not between 2 and 100 it should display 'Price must be numeric between 2 and 100e'
