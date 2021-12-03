104ef2b92ec0594792d26a9b45dc9c115e5c1cc6
simon <fctinstean@gmail.com>
2021-12-03 11:52:19 +0100

    Model solution v1

---
+++ MVC & Laravel/Day 4 - Model/Exercises/Model/BookController.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreBookRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\Book;
+
+class BookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $books = Book::all();
+
+        return view('books', ['books' => $books]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('new-book');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreBookRequest $request)
+    {
+        // Validations
+        $request->validated();
+
+        $book = new Book;
+        $book->title = $request->title;
+        $book->price = $request->price;
+        $book->author_id = $request->author_id;
+
+        if ($book->save())
+            return back()->with('success', 'Saved the book in the DB');
+        else
+            return back()->with('error', 'Something wrong with the DB.');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return 'Display book with id : ' . $id;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $book = Book::find($id);
+
+        return view('edit-book', ['book' => $book]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(StoreBookRequest $request, $id)
+    {
+        // Validations
+        $request->validated();
+
+        $book = Book::find($id);
+        $book->title = $request->title;
+        $book->price = $request->price;
+        $book->author_id = $request->author_id;
+
+        if ($book->save())
+            return back()->with('success', 'Updated in the DB');
+        else
+            return back()->with('error', 'Something wrong with the DB');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        /*$book = Book::find($id);
+        $book->delete();*/
+        $result = Book::destroy($id);
+
+        if ($result)
+            return back()->with('success', 'Book was deleted from the DB');
+        else
+            return back()->with('error', 'Something wrong with the DB.');
+    }
+}
---
+++ MVC & Laravel/Day 4 - Model/Exercises/Model/views/books.blade.php
@@ -0,0 +1,28 @@
+@extends('mytemplate')
+
+@section('title', 'Books List')
+
+@section('content')
+    <h2>Books</h2>
+
+    @if ($message = Session::get('success'))
+        <p style="color:green">{{ $message }}</p>
+    @endif
+
+    @if ($message = Session::get('error'))
+        <p style="color:red">{{ $message }}</p>
+    @endif
+
+    @if (!empty($books))
+        @foreach ($books as $book)
+            <strong>Title : </strong>{{ $book->title }}<br>
+            <strong>Price : </strong>{{ $book->price }}<br>
+            <a href="{{ route('books.details', [$book->id]) }}">Detail page</a><br>
+            <a href="{{ route('books.edit', [$book->id]) }}">Edit</a><br>
+            <a href="{{ route('books.delete', [$book->id]) }}">Delete</a>
+            <hr>
+        @endforeach
+    @else
+        <p>No books in my DB.</p>
+    @endif
+@endsection
