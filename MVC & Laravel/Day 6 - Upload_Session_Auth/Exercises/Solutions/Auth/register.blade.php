377acc74f132742f1bf1dd4cf8446a929d184b37
Simon <fctinstean@gmail.com>
2021-12-07 15:49:01 +0100

    Create register.blade.php

---
+++ MVC & Laravel/Day 6 - Upload_Session_Auth/Exercises/Solutions/Auth/register.blade.php
@@ -0,0 +1,88 @@
+<x-guest-layout>
+    <x-auth-card>
+        <x-slot name="logo">
+            <a href="/">
+                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+            </a>
+        </x-slot>
+
+        <!-- Validation Errors -->
+        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+
+        <form method="POST" action="{{ route('register') }}">
+            @csrf
+
+            <!-- First Name -->
+            <div>
+                <x-label for="first_name" :value="__('First Name')" />
+
+                <x-input id="first_name" class="block mt-1 w-full" type="text" name="first_name"
+                    :value="old('first_name')" required autofocus />
+            </div>
+
+            <!-- Last Name -->
+            <div>
+                <x-label for="last_name" :value="__('Last Name')" />
+
+                <x-input id="last_name" class="block mt-1 w-full" type="text" name="last_name" :value="old('last_name')"
+                    required autofocus />
+            </div>
+
+            <!-- City -->
+            <div>
+                <x-label for="city" :value="__('City')" />
+
+                <x-input id="city" class="block mt-1 w-full" type="text" name="city" :value="old('city')" required
+                    autofocus />
+            </div>
+
+            <!-- Postal code -->
+            <div>
+                <x-label for="postal_code" :value="__('Postal Code')" />
+
+                <x-input id="postal_code" class="block mt-1 w-full" type="number" name="postal_code"
+                    :value="old('postal_code')" required autofocus />
+            </div>
+
+            <!-- Postal code -->
+            <div>
+                <x-label for="role" :value="__('Role')" />
+
+                <x-input id="role" class="block mt-1" type="checkbox" name="role" :value="old('role')" autofocus />
+            </div>
+
+            <!-- Email Address -->
+            <div class="mt-4">
+                <x-label for="email" :value="__('Email')" />
+
+                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
+            </div>
+
+            <!-- Password -->
+            <div class="mt-4">
+                <x-label for="password" :value="__('Password')" />
+
+                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required
+                    autocomplete="new-password" />
+            </div>
+
+            <!-- Confirm Password -->
+            <div class="mt-4">
+                <x-label for="password_confirmation" :value="__('Confirm Password')" />
+
+                <x-input id="password_confirmation" class="block mt-1 w-full" type="password"
+                    name="password_confirmation" required />
+            </div>
+
+            <div class="flex items-center justify-end mt-4">
+                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
+                    {{ __('Already registered?') }}
+                </a>
+
+                <x-button class="ml-4">
+                    {{ __('Register') }}
+                </x-button>
+            </div>
+        </form>
+    </x-auth-card>
+</x-guest-layout>
